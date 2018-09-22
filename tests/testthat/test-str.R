96cf9cc5909b213ef619f388fd00a59e4eec8412
Edward Visel <alistaire@gmail.com>
2018-09-22 04:21:43 -0400

    work, tests, work

--- tests/testthat/test-str.R
+++ tests/testthat/test-str.R
@@ -45,7 +45,7 @@ test_that("glimpse text input can be read", {
 quoted_df <- tibble::data_frame(x = rep('test", "test', 10))
 test_that("quoted strings are well-handled", {
     expect_equivalent(
-        read_glimpse(capture.output(tibble::glimpse(quoted_df, width = 47))),
+        read_glimpse(capture.output(tibble::glimpse(quoted_df, width = 49))),
         quoted_df[1:2, ]
     )
     expect_equivalent(
